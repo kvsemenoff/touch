48f632a79c994077b6292ddaf227841373758d62
Konstantin Semenov <kvsemenoff@mail.ru>
2017-02-04 17:28:00 +0300

    d

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 ﻿<?php require_once('includes/header.php'); ?>
-	
+<?php require_once('includes/header_top.php'); ?>
+<?php require_once('includes/header_bottom.php'); ?>
+<?php require_once('includes/about.php'); ?>
+<?php require_once('includes/nabory.php'); ?>
 <?php require_once('includes/footer.php'); ?>
 
 	
-</body>
-</html>
 
---
+++ quick.php
@@ -0,0 +1,4 @@
+<?php require_once('includes/header.php'); ?>
+<?php require_once('includes/header_bottom2.php'); ?>
+
+<?php require_once('includes/footer.php'); ?>
