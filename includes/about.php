8185f06b320506e1494b7679309734844fab87ea
Barasbi <betokov93@gmail.com>
2017-02-06 17:48:30 +0300

    about

--- includes/about.php
+++ includes/about.php
@@ -13,12 +13,12 @@
         <div class="row">                  
             <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
                 <h2>О сервисе</h2>
-                <div clsss="dc-about-text">
+                <div class="dc-about-text">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </div>
             </div>    
         </div>
-    </div>    
+    </div>
 
     <div class="container">
         <div class="row">                    
@@ -27,36 +27,47 @@
             </div>
             <div class="col-md-3 col-lg-3 col-sm-12 col-xs-12">                 
                 <img src="images/dc-about-make-order.png">
-                <strong>Делаете заказ</strong>
+                <div class="dc-about-title"><strong>Делаете заказ</strong></div>
                 <hr class="dc-about-hr">
-                <div class="dc-about-text">
+                <div class="dc-about-text2">
                     Вы выбираете и добавляете в корзину интересующий Вас товар
                 </div>
             </div>    
             <div class="col-md-3 col-lg-3 col-sm-12 col-xs-12">
                 <img src="images/dc-about-register-order.png">
-                <strong>Оформляете заказ</strong>
+                <div class="dc-about-title"><strong>Оформляете заказ</strong></div>
                 <hr class="dc-about-hr">
-                <div class="dc-about-text">
+                <div class="dc-about-text2">
                     Вводите Вашу контактную информацию и число доставки
                 </div>
             </div>    
             <div class="col-md-3 col-lg-3 col-sm-12 col-xs-12">
                 <img src="images/dc-about-make-payment.png">
-                <strong>Проводите оплату</strong>
+                <div class="dc-about-title"><strong>Проводите оплату</strong></div>
                 <hr class="dc-about-hr">
-                <div class="dc-about-text">
+                <div class="dc-about-text2">
                     Выбираете способ оплаты: банковской картой или оплатой при получении
                 </div>
             </div>
             <div class="col-md-3 col-lg-3 col-sm-12 col-xs-12">
                 <img src="images/dc-about-receive-order.png">
-                <strong>Получаете Ваш заказ</strong>
+                <div class="dc-about-title"><strong>Получаете Ваш заказ</strong></div>
                 <hr class="dc-about-hr">
-                <div class="dc-about-text">
+                <div class="dc-about-text2">
                     Получаете заказ по выбранному Вами адресу
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
+                <form action="#" method="POST">
+                    <input type="submit" class="button-2" name="">
+                </form>
+            </div>
+        </div>
+    </div>
+
 </section>
