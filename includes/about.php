193ff5b5b00d79bd0ca4c33e4d407009badba0de
Barasbi <betokov93@gmail.com>
2017-02-06 17:15:37 +0300

    about

--- includes/about.php
+++ includes/about.php
@@ -5,7 +5,7 @@
                 <form action="#" method="POST">
                     <input type="submit" class="button-1" name="">
                 </form>
-            </div>    
+            </div>
         </div>
     </div>
 
@@ -13,7 +13,9 @@
         <div class="row">                  
             <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
                 <h2>О сервисе</h2>
-                <div clsss="dc-about-text"></div>
+                <div clsss="dc-about-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
             </div>    
         </div>
     </div>    
