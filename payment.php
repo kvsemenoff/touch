428cf2261fa59b3363f702748787b1a49907d8e9
ibragimtokmak <tokmakovibragim@gmail.com>
2017-02-06 14:57:35 +0300

    f

--- payment.php
+++ payment.php
@@ -1,4 +1,6 @@
 <?php require_once('includes/header.php'); ?>
-<?php require_once('includes/header_bottom2.php'); ?>
-
+<?php /*require_once('includes/header_bottom2.php'); */?>
+<section class="payment">
+    <div class="container"></div>    
+</section>
 <?php require_once('includes/footer.php'); ?>
