176cdd73b45f348171e85ffa7d9a8e5418af7b9f
Barasbi <betokov93@gmail.com>
2017-02-06 11:27:54 +0300

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,21 @@
-
-</body>
-</html>
+<section class="section section_padding dc_footer_view">
+    <hr class="dc_footer_hr">    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-lg-3 col-sm-12 col-xs-12">
+                <a href="#">Как это работает</a>
+            </div>    
+            <div class="col-md-3 col-lg-3 col-sm-12 col-xs-12">
+                <a href="#">Контакты</a>
+            </div>    
+            <div class="col-md-3 col-lg-3 col-sm-12 col-xs-12">
+                <a href="#">Условия использования</a>
+            </div>
+            <div class="col-md-3 col-lg-3 col-sm-12 col-xs-12">
+                <a href="#"><img src=""></a>
+                <a href="#"><img src=""></a>
+            </div>    
+        </div>
+    </div>
+</section>	
+	
