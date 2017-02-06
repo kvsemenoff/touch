a83fda9d107aa3fc2f164b6a0769c4ad7edd9b6b
Ilya <cuprum19930@gmail.com>
2017-02-06 19:42:07 +0500

    header_top.php

---
+++ includes/header_top.php
@@ -0,0 +1,26 @@
+<div class="container">
+    <header class="header">
+        <div class="header__left_wrapper header__inner_wrapper">
+            <a href="#" class="header__logo"></a>
+            <div class="header__lang">
+                <a href="#" class="header__language_btn">
+                    <span class="language_text">ru</span>
+                </a>
+                <ul class="header__language_list">
+                    <li class="header__language_item"><a href="#" class="language__option">ru</a></li>
+                    <li class="header__language_item"><a href="#" class="language__option">en</a></li>
+                </ul>
+            </div>
+
+
+        </div>
+        <div class="header__right_wrapper header__inner_wrapper">
+
+            <a href="mailto:example@mail.com" class="header__mail"><div class="header__mailing_icon"></div>
+                example@mail.com</a>
+            <a href="#" class="header__basket"></a>
+            <a href="#" class="header__menu_mobile"></a>
+        </div>
+
+    </header>
+</div>
