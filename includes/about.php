4f6523f7292abf7f6770c89aa97fb425a9ef9d8e
Barasbi <betokov93@gmail.com>
2017-02-06 16:30:37 +0300

    about

--- includes/about.php
+++ includes/about.php
@@ -1,1 +1,60 @@
-﻿
+﻿<section class="section dc_about_view">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
+                <form action="#" method="POST">
+                    <input type="submit" class="button-1" name="">
+                </form>
+            </div>    
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">                  
+            <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
+                <h2>О сервисе</h2>
+                <div clsss="dc-about-text"></div>
+            </div>    
+        </div>
+    </div>    
+
+    <div class="container">
+        <div class="row">                    
+            <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
+                <h2>Как мы работаем</h2>
+            </div>
+            <div class="col-md-3 col-lg-3 col-sm-12 col-xs-12">                 
+                <img src="images/dc-about-make-order.png">
+                <strong>Делаете заказ</strong>
+                <hr class="dc-about-hr">
+                <div class="dc-about-text">
+                    Вы выбираете и добавляете в корзину интересующий Вас товар
+                </div>
+            </div>    
+            <div class="col-md-3 col-lg-3 col-sm-12 col-xs-12">
+                <img src="images/dc-about-register-order.png">
+                <strong>Оформляете заказ</strong>
+                <hr class="dc-about-hr">
+                <div class="dc-about-text">
+                    Вводите Вашу контактную информацию и число доставки
+                </div>
+            </div>    
+            <div class="col-md-3 col-lg-3 col-sm-12 col-xs-12">
+                <img src="images/dc-about-make-payment.png">
+                <strong>Проводите оплату</strong>
+                <hr class="dc-about-hr">
+                <div class="dc-about-text">
+                    Выбираете способ оплаты: банковской картой или оплатой при получении
+                </div>
+            </div>
+            <div class="col-md-3 col-lg-3 col-sm-12 col-xs-12">
+                <img src="images/dc-about-receive-order.png">
+                <strong>Получаете Ваш заказ</strong>
+                <hr class="dc-about-hr">
+                <div class="dc-about-text">
+                    Получаете заказ по выбранному Вами адресу
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
