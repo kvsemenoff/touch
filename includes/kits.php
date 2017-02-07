e97e10fde37201db339328c29ee51a61f5f07ffe
Barasbi <betokov93@gmail.com>
2017-02-07 10:41:21 +0300

    nabory

---
+++ includes/kits.php
@@ -0,0 +1,29 @@
+﻿<section class="section dc_nabory_view">
+    <div class="container">
+        <div class="row">                  
+            <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
+                <h2>Наборы для регат</h2>
+                <div class="dc-nabory-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+            </div>    
+        </div>
+    </div>
+
+    <div class="dc-nabory-select_and_choose">
+	    <div class="container">
+	        <div class="row">                  
+	            <div class="col-md-3 col-lg-3 col-md-offset-3 col-lg-offset-3 col-sm-12 col-xs-12">
+	                <div class="dc-nabory-choose">
+	                    Выберите регату
+	                </div>        
+	            </div>
+	            <div class="col-md-2 col-lg-2 col-sm-12 col-xs-12">
+	                <select class="select-1">
+	                	<option>Регата №1</option>
+	                </select>
+	            </div>
+	        </div>
+	    </div>
+    </div>
+</section>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php require_once('includes/header_top.php'); ?>
 <?php require_once('includes/header_bottom.php'); ?>
 <?php require_once('includes/about.php'); ?>
-<?php require_once('includes/nabory.php'); ?>
+<?php require_once('includes/kits.php'); ?>
 <?php require_once('includes/footer.php'); ?>
 
 	
