8871ac0a0a4a2d3b7e81d90e53cc5cfcc865f2d2
Barasbi <betokov93@gmail.com>
2017-02-08 15:18:52 +0300

    buttons

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <?php require_once('includes/header_top.php'); ?>
 <?php require_once('includes/header_bottom.php'); ?>
 <?php require_once('includes/about.php'); ?>
+<?php require_once('includes/owr_kits.php'); ?>
 <?php require_once('includes/kits.php'); ?>
 <?php require_once('includes/kits2.php'); ?>
 <?php require_once('includes/quick_order.php'); ?>
